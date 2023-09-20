934c08e4136d770829fd8be6ea3ce6162e8c7227
UTFPR-FelipeFS <lipe.sorrilha@gmail.com>
2023-09-20 12:57:32 -0300

    Update Pokedex2.c

--- Pokedex2.c
+++ Pokedex2.c
@@ -24,15 +24,15 @@ typedef struct Pokemon{
     int prxEvo; // Próxima evolução
 } Pokemon;
 
-#define MAX_POKEMON 722 // Defina o tamanho máximo da lista de Pokémon
+#define MAX_POKEMON 722 
 
-int main() {
+int main(){
     FILE *arquivo;
-    char linha[512]; // Assuma que as linhas do arquivo CSV têm no máximo 512 caracteres
-    Pokemon listaPokemon[MAX_POKEMON]; // Armazena os Pokémon lidos do arquivo CSV
-    int contador = 0; // Contador para rastrear o número de Pokémon lidos
+    char linha[512]; 
+    Pokemon listaPokemon[MAX_POKEMON];
+    int contador = 0;
 
-    arquivo = fopen("C:/Users/Huelipe/Documents/GitHub/projetoPokemon/pokedex.csv", "r"); // Substitua pelo caminho correto do seu arquivo CSV
+    arquivo = fopen("C:/Users/Huelipe/Documents/GitHub/projetoPokemon/pokedex.csv", "r");
 
     if (arquivo == NULL) {
         perror("Erro ao abrir o arquivo");
@@ -40,18 +40,14 @@ int main() {
     }
 
     while (fgets(linha, sizeof(linha), arquivo)) {
-        // Use strtok ou sscanf para extrair os valores da linha
         char *token = strtok(linha, ",");
         if (token != NULL) {
             listaPokemon[contador].nPokedex = atoi(token);
-            // Repita para os outros membros da estrutura, seguindo a ordem dos campos no arquivo CSV
             token = strtok(NULL, ",");
             if (token != NULL) {
                 strncpy(listaPokemon[contador].nome, token, sizeof(listaPokemon[contador].nome));
             }
-            // Continue para os outros membros...
         }
-        // Incrementa o contador
         contador++;
     }
     fclose(arquivo);
