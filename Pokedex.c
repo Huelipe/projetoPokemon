5742501a04773f5270ffa52f2bd8e53851151899
UTFPR-FelipeFS <lipe.sorrilha@gmail.com>
2023-09-20 12:36:38 -0300

    Update Pokedex.c

---
+++ Pokedex.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h> 
+
+typedef struct Pokemon{
+    int nPokedex; // Número na pokedex
+    char nome[40]; // Nome do pokémon
+    char tipo1[10]; // Primeiro tipo do pokémon
+    char tipo2[10]; // Segundo tipo do pokémon (dever ser registrado "NULL" caso não tenha)
+    int total; // Soma total dos status base
+    int hp; // Tanto de vida
+    int atk; // Tanto de ataque
+    int def; // Tanto de defesa
+    int spatack; // Tanto de ataque especial
+    int spdef; // Tanto de defesa especial
+    int speed; // Tanto de velocidade
+    int geracao; // Geração a qual pertence o pokemon
+    int lendario; // Define se é lendario ou não a partir de booleano
+    char cor[10]; // Cor do pokemon
+    float altura; // Altura do pokemon
+    float peso; // Peso do pokemon
+    int captura; // Taxa de captura
+    int preEvo; // Pré evolução
+    int prxEvo; // Próxima evolução
+} Pokemon;
+
+#define MAX_POKEMON 722 // Defina o tamanho máximo da lista de Pokémon
+
+int main() {
+    FILE *arquivo;
+    char linha[512]; // Assuma que as linhas do arquivo CSV têm no máximo 512 caracteres
+    Pokemon listaPokemon[MAX_POKEMON]; // Armazena os Pokémon lidos do arquivo CSV
+    int contador = 0; // Contador para rastrear o número de Pokémon lidos
+
+    arquivo = fopen("C:/Users/Huelipe/Documents/PokemonVSCODE/pokedex.csv", "r"); // Substitua "seuarquivo.csv" pelo nome do seu arquivo CSV
+
+    if (arquivo == NULL) {
+        perror("Erro ao abrir o arquivo");
+        return 1;
+    }
+
+    while (fgets(linha, sizeof(linha), arquivo)) {
+        // Use strtok ou sscanf para extrair os valores da linha
+        // Exemplo com strtok:
+        char *token = strtok(linha, ",");
+        if (token != NULL) {
+            listaPokemon[contador].nPokedex = atoi(token);
+        }
+        // Repita para os outros membros da estrutura
+        // Incrementa o contador
+        contador++;
+    }
+
+    fclose(arquivo);
+
+
+    return 0;
+}
