5e737bcb45140e17f38a0d55ea549ad738c1a496
UTFPR-FelipeFS <lipe.sorrilha@gmail.com>
2023-10-07 00:42:27 -0300

    Update Data.c

--- Data.c
+++ Data.c
@@ -242,6 +242,75 @@ int main(){
     strcpy(natures[24].nome, "Quiet");
     strcpy(natures[25].nome, "Sassy");
 
+    float comum = 1;
+    float mais = 1.1;
+    float menos = 0.9;
+
+    natures[1].modAtk = natures[1].modDef = natures[1].modSpatack = natures[1].modSpdef = natures[1].modSpeed = comum;
+    natures[2].modAtk = natures[2].modDef = natures[2].modSpatack = natures[2].modSpdef = natures[2].modSpeed = comum;
+    natures[3].modAtk = natures[3].modDef = natures[3].modSpatack = natures[3].modSpdef = natures[3].modSpeed = comum;
+    natures[4].modAtk = natures[4].modDef = natures[4].modSpatack = natures[4].modSpdef = natures[4].modSpeed = comum;
+    natures[5].modAtk = natures[5].modDef = natures[5].modSpatack = natures[5].modSpdef = natures[5].modSpeed = comum;
+        natures[6].modDef = mais;
+            natures[6].modAtk = menos;
+                natures[6].modSpdef = natures[6].modSpatack = natures[6].modSpeed = comum;
+        natures[7].modSpatack = mais;
+            natures[7].modAtk = menos;
+                natures[7].modSpdef = natures[7].modDef = natures[7].modSpeed = comum;
+        natures[8].modSpdef = mais;
+            natures[8].modAtk = menos;
+                natures[8].modDef = natures[8].modSpatack = natures[8].modSpeed = comum;
+        natures[9].modSpeed = mais;
+            natures[9].modAtk = menos;
+                natures[9].modSpdef = natures[9].modSpatack = natures[9].modDef = comum;
+        natures[10].modAtk = mais;
+            natures[10].modDef = menos;
+                natures[10].modSpdef = natures[10].modSpatack = natures[10].modSpeed = comum;       
+        natures[11].modSpatack = mais;
+            natures[11].modDef = menos;
+                natures[11].modSpdef = natures[11].modAtk = natures[11].modSpeed = comum;
+        natures[12].modSpdef = mais;
+            natures[12].modDef = menos;
+                natures[12].modAtk = natures[12].modSpatack = natures[12].modSpeed = comum;
+        natures[13].modSpeed = mais;
+            natures[13].modDef = menos;
+                natures[13].modSpdef = natures[13].modSpatack = natures[13].modAtk = comum;
+        natures[14].modAtk = mais;
+            natures[14].modSpatack = menos;
+                natures[14].modSpdef = natures[14].modDef = natures[14].modSpeed = comum;
+        natures[15].modDef = mais;
+            natures[15].modSpatack = menos;
+                natures[15].modSpdef = natures[15].modAtk = natures[15].modSpeed = comum;
+        natures[16].modSpdef = mais;
+            natures[16].modSpatack = menos;
+                natures[16].modDef = natures[16].modAtk = natures[16].modSpeed = comum;
+        natures[17].modSpeed = mais;
+            natures[17].modSpatack = menos;
+                natures[17].modSpdef = natures[17].modAtk = natures[17].modDef = comum;
+        natures[18].modAtk = mais;
+            natures[18].modSpdef = menos;
+                natures[18].modDef = natures[18].modSpatack = natures[18].modSpeed = comum;
+        natures[19].modDef = mais;
+            natures[19].modSpdef = menos;
+                natures[19].modAtk = natures[19].modSpatack = natures[19].modSpeed = comum;
+        natures[20].modSpatack = mais;
+            natures[20].modSpdef = menos;
+                natures[20].modAtk = natures[20].modAtk = natures[20].modSpeed = comum;
+        natures[21].modSpeed = mais;
+            natures[21].modSpdef = menos;
+                natures[21].modAtk = natures[21].modAtk = natures[21].modSpatack = comum;
+        natures[22].modAtk = mais;
+            natures[22].modSpeed = menos;
+                natures[22].modSpdef = natures[22].modSpatack = natures[22].modDef = comum;
+        natures[23].modDef = mais;
+            natures[23].modSpeed = menos;
+                natures[23].modSpdef = natures[23].modSpatack = natures[23].modAtk = comum;
+        natures[24].modSpatack = mais;
+            natures[24].modSpeed = menos;
+                natures[24].modSpdef = natures[24].modAtk = natures[24].modDef = comum;
+        natures[25].modSpdef = mais;
+            natures[25].modSpeed = menos;
+                natures[25].modAtk = natures[25].modSpatack = natures[25].modDef = comum;
 
     printf("Digite o nome do pokemon que você está batalhando!");
     char nome[40];
@@ -249,7 +318,7 @@ int main(){
     fgets(nome, 39, stdin);
     nome[strcspn(nome, "\n")] = '\0';
 
-    Pokemon selvagem;
+    Pokemon selvagagemNaDex;
     pokemonCapturado selvagem;
 
     for(int i = 0; i++; i < 722){
