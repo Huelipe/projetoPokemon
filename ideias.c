4b3eb86f668c9709daa60c210075de7ffd99f2d8
UTFPR-FelipeFS <145513328+UTFPR-Felipe-Sorrilha@users.noreply.github.com>
2023-10-25 17:27:57 -0300

    Update ideias.c

--- ideias.c
+++ ideias.c
@@ -60,8 +60,6 @@ pronto
 Anotações:
 https://github.com/veekun/pokedex/blob/master/pokedex/data/csv/pokemon_species.csv
 Obter: Id, numero para qual pokemon evolui e ?
-https://github.com/veekun/pokedex/blob/master/pokedex/data/csv/pokemon_evolution.csv
-Para evoluções por nivel
-As que não tiver nível serão evoluidas em um nivel x
+Definir numero base para evolução 1 e 2 
 */
 
